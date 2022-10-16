6750d054cfcee851ea71b1572d2f3363d61ff555
edgar-rosales95 <95332608+edgar-rosales95@users.noreply.github.com>
2022-10-16 10:56:15 -0700

    added in text box for nickname with some pizaz

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,3 +1,8 @@
+import 'package:bullshit/main.dart';
+import 'package:bullshit/responsive/responsive.dart';
+import 'package:bullshit/widgets/custom_button.dart';
+import 'package:bullshit/widgets/custom_text.dart';
+import 'package:bullshit/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
 class CreateRoomScreen extends StatefulWidget {
@@ -9,8 +14,48 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void disposr() {
+    // TODO: implement dispose
+    super.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 90,
+                    color: Colors.blue,
+                  ),
+                ],
+                text: 'Create Room',
+                fontSize: 60,
+              ),
+              SizedBox(height: size.height * 0.08),
+              CustomTextField(
+                  controller: _nameController, hintText: 'enter your nickname'),
+              SizedBox(height: size.height * 0.30),
+              CustomButton(onTap: () {}, text: 'create'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,31 @@
+import 'package:bullshit/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  const CustomTextField(
+      {Key? key, required this.controller, required this.hintText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.blue,
+          blurRadius: 5,
+          spreadRadius: 5,
+        )
+      ]),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          fillColor: bgColor,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
