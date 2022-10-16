784b5a1fd327509af3b1839092f830d1eccb259b
Dominic Fanucchi <dominic_fanucchi@yahoo.com>
2022-10-16 02:19:42 -0700

    adding firebase configuration

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@
 // importing modules
 const express = require('express');
 const http = require('http');
+const firebase = require('firebase');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,9 +17,24 @@ var io = require('socket.io')(server);
 // middleware
 app.use(express.json());
 
-const DB = "";
-// PROMISE in JS == FUTURE in Dart
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBL2lJQ88kZgDNx8wGJkFEUmlSsTqeIjJ0",
+    authDomain: "calhacks-2022-e619e.firebaseapp.com",
+    projectId: "calhacks-2022-e619e",
+    storageBucket: "calhacks-2022-e619e.appspot.com",
+    messagingSenderId: "704984036602",
+    appId: "1:704984036602:web:2ed97641e4501dba438543"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+//const DB = firebase.DB();
+// PROMISE in JS == FUTURE in Dart
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server started and running on ${port}`);
 });
+
+
